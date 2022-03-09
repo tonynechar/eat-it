ce4b2af848db2a09fa045fd6484d2a2a7cd78557
Tony Nechar <t.nechar@gmail.com>
2022-03-09 20:46:16 +0100

    fixed some general bugs, modified  controller functions for running the app in demo mode

--- client/src/Components/RestaurantList/RestaurantList.js
+++ client/src/Components/RestaurantList/RestaurantList.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import RestaurantItem from '../RestaurantItem/RestaurantItem';
+import { v4 as uuidv4 } from 'uuid';
 import './RestaurantList.css';
 
 function RestaurantList({ restaurants, category }) {
 
   const categorizedRestaurants = restaurants.map(restaurant => {
     if (restaurant.categories.includes(category) || category === '') {
-      return <RestaurantItem key={restaurant._id} restaurant={restaurant} />;
+      return <RestaurantItem key={uuidv4()} restaurant={restaurant} />;
     }
     return null;
   });
--- server/controllers/restaurant.js
+++ server/controllers/restaurant.js
@@ -1,25 +1,64 @@
-const { add } = require('../models/dish');
 const restaurantModel = require('../models/restaurant');
+const mockData = require('../demo-db.json');
 
 const getRestaurants = async (req, res) => {
+  // START PRODUCTION MODE
+
+  // try {
+  //   const restaurants = await restaurantModel.find();
+  //   res.status(200)
+  //   res.send(restaurants);
+  // } catch (err) {
+  //   res.status(500).send(err);
+  // }
+
+  // END PRODUCTION MODE
+
+  //-------------------
+
+  // START DEMO MODE
+
   try {
-    const restaurants = await restaurantModel.find();
-    res.status(200)
-    res.send(restaurants);
+    res.status(200);
+    res.send(mockData);
   } catch (err) {
     res.status(500).send(err);
   }
+
+  // END DEMO MODE
 }
 
 const getOneRestaurant = async (req, res) => {
+  // START PRODUCTION MODE
+
+  // try {
+  //   const restaurantId = req.params.id;
+  //   const restaurant = await restaurantModel.findOne({ _id: restaurantId });
+  //   res.status(200);
+  //   res.send(restaurant);
+  // } catch (err) {
+  //   res.status(404).send(err);
+  // }
+
+  // END PRODUCTION MODE
+
+  //-------------------
+
+  // START DEMO MODE
+
   try {
     const restaurantId = req.params.id;
-    const restaurant = await restaurantModel.findOne({ _id: restaurantId });
-    res.status(200);
-    res.send(restaurant);
+    for (let i = 0; i < mockData.length; i++) {
+      if (mockData[i]._id === restaurantId) {
+        res.status(200);
+        res.send(mockData[i]);
+      }
+    }
   } catch (err) {
-    res.status(404).send(err);
+    res.status(500).send(err);
   }
+
+  // END DEMO MODE
 }
 
 const createRestaurant = async (req, res) => {
@@ -34,7 +73,13 @@ const createRestaurant = async (req, res) => {
 }
 
 const createOrder = async (req, res) => {
-  // REFACTOR AND IMPLEMENT DATA MODEL HERE
+  try {
+    const urlEncodedOrder = `https://wa.me/529995479695/?text=You%20are%20in%20demo%20mode`;
+    res.status(201);
+    res.json(urlEncodedOrder);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 }
 
 module.exports = { getRestaurants, getOneRestaurant, createRestaurant, createOrder };
